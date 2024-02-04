7c7ecd53515d49cd872ae8342f272eb393182be9
Glib Alieksieiev <gliba@wix.com>
2024-02-04 20:26:23 +0200

    add cache-oblivious solution

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,29 @@
 #include "solution.hpp"
 #include <algorithm>
 
-const int TILE_SIZE = 16;
+#ifdef CACHE_OBLIVIOUS
+const int TILE_SIZE = 4;
+void recurse(MatrixOfDoubles &in, MatrixOfDoubles &out, int r, int c, int size) {
+  if (size <= TILE_SIZE) {
+    for (int i = r; i < r + size; i++) {
+      for (int j = c; j < c + size; j++) {
+        out[i][j] = in[j][i];
+      }
+    }
+  } else {
+    int newSize = size / 2;
+    recurse(in, out, r, c, newSize);
+    recurse(in, out, r, c + newSize, size - newSize);
+    recurse(in, out, r + newSize, c, size - newSize);
+    recurse(in, out, r + newSize, c + newSize, size - newSize);
+  }
+}
+#endif
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
 #ifdef SOLUTION
+  const int TILE_SIZE = 16;
   for (int r = 0; r < size; r += TILE_SIZE) {
     for (int c = 0; c < size; c += TILE_SIZE) {
       for (int i = r; i < std::min(r + TILE_SIZE, size); i++) {
@@ -15,6 +33,8 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
       }
     }
   }
+#elif defined(CACHE_OBLIVIOUS)
+  recurse(in, out, 0, 0, size);
 #else
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
