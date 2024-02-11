f254a4ce43de198152e7349669b04cc5cba05313
Glib Alieksieiev <gliba@wix.com>
2024-02-11 15:29:49 +0200

    results

--- scripts/start-droplet.sh
+++ scripts/start-droplet.sh
@@ -7,3 +7,8 @@ doctl compute droplet create perf-ninja \
   --ssh-keys "4e:e3:92:1c:c9:56:51:99:bc:9f:27:aa:8a:e8:ad:98" \
   --verbose \
   --wait 
+
+# use --size c-4 if more memory or cores is needed.
+# note that c-2 is essentially 1-core instance because DigitalOcean uses hyperthreading.
+# this is the reason why false sharing lab doesn't work as expected on c-2 instance (TODO check on c-4)
+# but works on my Mac.
