c243710f0e4c64ec14fc372d2121378490f92103
Glib Alieksieiev <gliba@wix.com>
2024-03-31 13:45:43 +0300

    final solution

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -1,28 +1,9 @@
 
 #include "solution.h"
 #include <algorithm>
-#include <stdlib.h>
-
-static int compare(const S& a, const S& b) {
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
-
-inline bool adaptedCompare(const S& a, const S& b) {
-  return compare(a, b) < 0;
-}
 
 void solution(std::array<S, N> &arr) {
-  std::sort(arr.begin(), arr.end(), adaptedCompare);
+  std::sort(arr.begin(), arr.end(), [](const S& a, const S& b) {
+    return a.key1 < b.key1 || (a.key1 == b.key1 && a.key2 < b.key2);
+  });
 }
