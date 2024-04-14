6833a4877d7797d11d9eedf8bbfbe203ae2b5c9a
Glib Alieksieiev <gliba@wix.com>
2024-04-14 21:25:45 +0300

    not sure if this is what was required, but it speeds things up

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,16 +6,23 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray as, bs, cs;
+
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            as.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+            bs.push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+            cs.push_back(std::make_unique<ClassC>());
         }
     }
+
+    array.reserve(as.size() + bs.size() + cs.size());
+    array.insert(array.end(), std::make_move_iterator(as.begin()), std::make_move_iterator(as.end()));
+    array.insert(array.end(), std::make_move_iterator(bs.begin()), std::make_move_iterator(bs.end()));
+    array.insert(array.end(), std::make_move_iterator(cs.begin()), std::make_move_iterator(cs.end()));    
 }
 
 // Invoke the `handle` method on all instances in `output`
